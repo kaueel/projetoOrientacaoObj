f4dca4aa57bf5145a2b56bd4506a5df402102ab8
kaueel <k177650@g.unicamp.br>
2017-04-07 20:24:31 -0300

    INIT

---
+++ cmake-build-debug/CMakeFiles/FT_Bakery.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Kaue/Documents/FT_Bakery/a02ex00.cpp" "C:/Users/Kaue/Documents/FT_Bakery/cmake-build-debug/CMakeFiles/FT_Bakery.dir/a02ex00.cpp.obj"
+  "C:/Users/Kaue/Documents/FT_Bakery/a02ex01_a.cpp" "C:/Users/Kaue/Documents/FT_Bakery/cmake-build-debug/CMakeFiles/FT_Bakery.dir/a02ex01_a.cpp.obj"
+  "C:/Users/Kaue/Documents/FT_Bakery/a02ex03.cpp" "C:/Users/Kaue/Documents/FT_Bakery/cmake-build-debug/CMakeFiles/FT_Bakery.dir/a02ex03.cpp.obj"
+  "C:/Users/Kaue/Documents/FT_Bakery/a02ex03_a.cpp" "C:/Users/Kaue/Documents/FT_Bakery/cmake-build-debug/CMakeFiles/FT_Bakery.dir/a02ex03_a.cpp.obj"
+  "C:/Users/Kaue/Documents/FT_Bakery/a02ex03_b.cpp" "C:/Users/Kaue/Documents/FT_Bakery/cmake-build-debug/CMakeFiles/FT_Bakery.dir/a02ex03_b.cpp.obj"
+  "C:/Users/Kaue/Documents/FT_Bakery/a02ex03_c.cpp" "C:/Users/Kaue/Documents/FT_Bakery/cmake-build-debug/CMakeFiles/FT_Bakery.dir/a02ex03_c.cpp.obj"
+  "C:/Users/Kaue/Documents/FT_Bakery/a02ex03_d.cpp" "C:/Users/Kaue/Documents/FT_Bakery/cmake-build-debug/CMakeFiles/FT_Bakery.dir/a02ex03_d.cpp.obj"
+  "C:/Users/Kaue/Documents/FT_Bakery/a02ex03_e.cpp" "C:/Users/Kaue/Documents/FT_Bakery/cmake-build-debug/CMakeFiles/FT_Bakery.dir/a02ex03_e.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
